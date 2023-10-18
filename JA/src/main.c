0fa4bf23b295b4303567e28ef6b5c4e50b3e3a4d
Juan Antonio <juanantonio@Mac-mini-de-Juan.local>
2023-10-18 15:16:37 +0200

    Gestion comillas tokenizer

--- JA/src/main.c
+++ JA/src/main.c
@@ -22,14 +22,13 @@ int main(int ac, char **av)
 		return (1);
 	while(42)
 	{
-		str = readline(BLUE_T"\nMiniShell:" YELLOW_T" $>"RESET_COLOR);
-  		//str ="Hello|World>A";
-		printf("%s", str);
+		//str = readline(BLUE_T"\nMiniShell:" YELLOW_T" $>"RESET_COLOR);
+  		str =">Hello|'World>A";
 		lexe = tokenizer(&lexe, str);	
 		t_lexer *test = lexe;
 		while (test != NULL)
 		{
-			printf("%s\n", test->str);
+			printf("%s===%u\n", test->str,test->token);
 			test = test->next;
 		}
 	}
--- JA/src/tokenizer/token_splitter.c
+++ JA/src/tokenizer/token_splitter.c
@@ -4,6 +4,7 @@
 static char		*ft_pipestrdupli(const char *s1, char cut);
 static int		ft_pipecntwrds(char const *s, char c);
 static char		**ft_pipesplipri(char **split, char const *s, char c);
+static char		**check_pipesplitter(char **split);
 
 char	**ft_pipesplit(char const *s, char c)
 {
@@ -21,12 +22,12 @@ char	**ft_pipesplit(char const *s, char c)
 	}
 	else
 		split[0] = NULL;
+	split = check_pipesplitter(split);
 	return (split);
 }
 
 static char	**ft_pipesplipri(char **split, char const *s, char c)
 {
-
 	int		i;
 	int		j;
 	int		words;
@@ -38,14 +39,13 @@ static char	**ft_pipesplipri(char **split, char const *s, char c)
 	{
 		if (s[i] == (unsigned char)c)
 			words = 1;
-		else if ((words == 1 && s[i] != (unsigned char)c) ||
-				(s[i] == 124 || s[i] == 60 || s[i] == 62))
+		else if ((words == 1 && s[i] != (unsigned char)c) || (s[i] == 124 || s[i] == 60 || s[i] == 6 || s[i] == 34 || s[i] == 39))
 		{
 			split[j] = ft_pipestrdupli(&s[i], c);
 			if (split[j] == NULL)
 				return (ft_freesp(split));
 			words = 0;
-			if (s[i] == 124 || s[i] == 60 || s[i] == 62)
+			if (s[i] == 124 || s[i] == 60 || s[i] == 62 || s[i] == 34 || s[i] == 39)
 				words = 1;
 			j++;
 		}
@@ -65,20 +65,20 @@ static int    ft_pipecntwrds(char const *s, char c)
     contl = 0;
     while (s[i] != 0)
     {
-        if (s[i] != c && s[i] != 124 && s[i] != 60 && s[i] != 62)
+        if (s[i] != c && s[i] != 124 && s[i] != 60 && s[i] != 62 && s[i] != 34 && s[i] != 39)
             contl++;
-        if ((s[i] == c || s[i] == 124 || s[i] == 60 || s[i] == 62) && contl > 0)
+        if ((s[i] == c || s[i] == 124 || s[i] == 60 || s[i] == 62 || s[i] == 34 || s[i] == 39) && contl > 0)
         {
             contw++;
             contl = 0;
         }
-        if ((s[i] == 124 || s[i] == 60 || s[i] == 62) && contl == 0)
+        if ((s[i] == 124 || s[i] == 60 || s[i] == 62 || s[i] == 34 || s[i] == 39) && contl == 0)
           contw++;
         i++;
     }
     if (contl > 0)
         contw++;
-    return (contw);
+	return (contw);
 }
 
 static    char    *ft_pipestrdupli(const char *s1, char cut)
@@ -91,18 +91,16 @@ static    char    *ft_pipestrdupli(const char *s1, char cut)
     str = s1;
     j = 0;
     i = 0;
-    if (str[i] == 124 || str[i] == 60 || str[i] == 62)
+    if (str[i] == 124 || str[i] == 60 || str[i] == 62 || str[i] == 34 || str[i] == 39)
       i++;
     else
     {
-      while ((str[i] != cut && str[i] != 0 && str[i] != 124 && str[i] != 60 && str[i] != 62))
+      while ((str[i] != cut && str[i] != 0 && str[i] != 124 && str[i] != 60 && str[i] != 62 && str[i] != 34 && str[i] != 39))
         i++;
     }
     dupstr = malloc(i * sizeof(char) + 1);
     if (!dupstr)
-    {
         return (NULL);
-    }
     while (j < i)
     {
         dupstr[j] = str[j];
@@ -110,5 +108,29 @@ static    char    *ft_pipestrdupli(const char *s1, char cut)
     }
     dupstr[j] = 0;
     return (dupstr);
+}
+
+static char		**check_pipesplitter(char **split)
+{
+	int i;
+	int quote;
+
+	i = 0;
+	quote = 0;
+	while (split[i])
+	{
+		if (ft_strchr(split[i], 34) || ft_strchr(split[i], 34)) 
+			quote = (int)split[i][0];
+		if (quote > 0)
+			break;
+	i++;
+	}
+	while (quote > 0 && split[i])
+	{
+		if (ft_strchr(split[i + 1], quote) == NULL)
+			split[i] = ft_strjoin_free(split[i + 1], split[i]);
+	i++;
+	}
+	return (split);
 }
 
