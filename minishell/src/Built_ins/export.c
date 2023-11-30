3182923b5e9e23ba1b4c53ab8890b33cb5bb17e8
jobort <jorbort92@gmail.com>
2023-11-30 17:35:16 +0100

    agregada la ultima built-in ya con el exit status tenido en cuenta

--- minishell/src/Built_ins/export.c
+++ minishell/src/Built_ins/export.c
@@ -3,16 +3,126 @@
 /*                                                        :::      ::::::::   */
 /*   export.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: juanantonio <juanantonio@student.42.fr>    +#+  +:+       +#+        */
+/*   By: jorge <jorge@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 12:44:41 by jbortolo          #+#    #+#             */
-/*   Updated: 2023/11/29 13:53:52 by juanantonio      ###   ########.fr       */
+/*   Updated: 2023/11/30 17:31:33 by jorge            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/minishell.h"
 
-int	ft_export(t_cmd *cmd_list)
+int	var_exist(t_program *program, char *str)
 {
+	int	i;
+
+	i = 0;
+	if (str[equal_sign(str)] == '\"')
+		trim_quotes(str,'\"');
+	if (str[equal_sign(str)] == '\'')
+		trim_quotes(str, '\'');
+	while (program->data->envp)
+	{
+		if (ft_strncmp(program->data->envp[i],
+						str, equal_sign(program->data->envp[i])) == 0)
+		{
+			free(program->data->envp[i]);
+			program->data->envp[i] = ft_strdup(str);
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+
+int	check_parameter(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (ft_isdigit(str[0]))
+		return (export_error(str));
+	if (equal_sign(str) == 0)
+		return (1);
+	if (str[0] != '=')
+		return (export_error(str));
+	while (str[i] != '=')
+	{
+		if (invalid_unsetchar(str[i]))
+			return (export_error(str));
+		i++;
+	}
+	return (0);
+}
+
+char	**whileloop_add_var(char **arr, char **rtn, char *str)
+{
+	int	i;
+
+	i = 0;
+	while (arr[i] != NULL)
+	{
+		if (arr[i + 1] == NULL)
+		{
+			rtn[i] = ft_strdup(str);
+			rtn[i + 1] = ft_strdup(arr[i]);
+		}
+		else 
+			rtn[i] = ft_strdup(arr[i]);
+		if (rtn[i] == NULL)
+		{
+			free_double_arr(rtn);
+			return (rtn);
+		}
+		i++;
+	}
+	return (rtn);
+}
+
+char	**add_var(char **arr, char *str)
+{
+	char	**rtn;
+	size_t	i;
+
+	i = 0;
+	if (str[equal_sign(str)] == '\"')
+		trim_quotes(str,'\"');
+	if (str[equal_sign(str)] == '\'')
+		trim_quotes(str, '\'');
+	while (arr[i] != NULL)
+		i++;
+	rtn = ft_calloc(sizeof(char *), i+2);
+	if (!rtn)
+		return (NULL);
+	i = 0;
+	whileloop_add_var(arr, rtn, str);
+	return (rtn);
+}
+
+int	ft_export(t_cmd *cmd_list, t_program *program)
+{
+	char **tmp;
+	int i = 0;
+
+	i = 1;
+	if (!cmd_list->cmd[1] || cmd_list->cmd[1][0] == '\0')
+		print_env(program);
+	else 
+	{
+		while (cmd_list->cmd[i])
+		{
+			if (check_input(cmd_list->cmd[i]) == 0
+					&& var_exist(program, cmd_list->cmd[i]) ==0)
+			{
+				if (cmd_list->cmd[i])
+				{
+					tmp = add_var(program->data->envp, cmd_list->cmd[i]);
+					free_double_arr(program->data->envp);
+					program->data->envp = tmp;
+				}
+			}
+			i++;
+		}
+	}
 	return (0);
 }
---
+++ minishell/src/Built_ins/export_utils.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   export_utils.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jorge <jorge@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/30 17:30:21 by jorge             #+#    #+#             */
+/*   Updated: 2023/11/30 17:32:52 by jorge            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "includes/minishell.h"
+
+int	export_error(char *c)
+{
+	ft_putstr_fd("minishell: export: ", STDERR_FILENO);
+	if (c)
+	{
+		ft_putchar_fd('\'',STDERR_FILENO);
+		ft_putstr_fd(c, STDERR_FILENO);
+		ft_putstr_fd("\': is", STDERR_FILENO);
+	}
+	ft_putendl_fd("not a valid identifier", STDERR_FILENO);
+	return (1);
+}
+
+size_t	equal_sign(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '=')
+			return (i + 1);
+		i++;
+	}
+	return (0);
+}
+
+char	*trim_quotes(char *str, char c)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (str[i])
+	{
+		if (str[i] == c)
+		{
+			j = 0;
+			while (str[i + j] == c)
+				j++;
+			ft_strlcpy(&str[i], &str[i + j], strlen(str) - i);
+		}
+		i++;
+	}
+	return (str);
+}
